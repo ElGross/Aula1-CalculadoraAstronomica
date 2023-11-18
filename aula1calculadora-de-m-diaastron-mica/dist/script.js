21a6f16475a56d1fb5009a9b0cd8d6f7569d42cc
ElGross <151382540+ElGross@users.noreply.github.com>
2023-11-18 12:13:34 -0300

    Conversor de anos-luz em metros

---
+++ aula1calculadora-de-m-diaastron-mica/dist/script.js
@@ -0,0 +1,5 @@
+var proximaCentauri = 4.22;
+var anoLuz = 9.1 * Math.pow(10, 13);
+var distanciaDaTerra = proximaCentauri * anoLuz;
+distanciaDaTerra = distanciaDaTerra;
+alert("Próxima Centauri fica a " + distanciaDaTerra + "m da Terra");
